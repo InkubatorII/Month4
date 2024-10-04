1889755036f35d2da292cfa5163d998f4b3a2887
InkubatorII <holloward@gmail.com>
2024-10-04 20:49:34 +0600

    lesson2

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,18 @@
+from random import choices
 from django.db import models
 
-# Create your models here.
+'''
+post.
+
+
+
+'''
+
+class Post(models.Model):
+    title = models.CharField(max_length=70)
+    content = models.TextField(null=True, blank=True)
+    rate = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
