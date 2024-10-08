1c8537e737fb9e7accc93a57247553e08336a268
InkubatorII <holloward@gmail.com>
2024-10-08 20:52:24 +0600

    lesson3.1

--- posts/models.py
+++ posts/models.py
@@ -11,11 +11,15 @@ class Comment(models.Model):
     text = models.CharField(max_length=250)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
 class Post(models.Model):
     image = models.ImageField(null=True, blank=True, upload_to='static/images/')
     title = models.CharField(max_length=70)
     content = models.TextField(null=True, blank=True)
     rate = models.IntegerField(default=0)
+    tags = models.ManyToManyField(Tag, related_name='posts')
 
     def __str__(self):
         return self.title
