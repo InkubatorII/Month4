f3ea61c12e177f7d52b0ddacf4ce8018c23cc78d
InkubatorII <holloward@gmail.com>
2024-10-08 20:41:53 +0600

    lesson3

--- posts/models.py
+++ posts/models.py
@@ -7,12 +7,17 @@ post.
 
 
 '''
+class Comment(models.Model):
+    text = models.CharField(max_length=250)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
 
 class Post(models.Model):
+    image = models.ImageField(null=True, blank=True, upload_to='static/images/')
     title = models.CharField(max_length=70)
     content = models.TextField(null=True, blank=True)
     rate = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
+
 
